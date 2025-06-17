bdc44c1b4b39c83beeb1672bdd1ba669497c0e0b
Bailey <64835549+IAmAGameDev@users.noreply.github.com>
2025-06-17 09:42:04 +0100

    Header with React

---
+++ src/Header.tsx
@@ -0,0 +1,81 @@
+import './styles/header.css';
+
+function Header() {
+  return (
+    <header>
+      <div className='header'>
+        <div className='header-left'>
+          <a
+            className='header-link'
+            href='index.html'
+            rel='noopener noreferrer'
+          >
+            <p className='header-name'>Bailey's Portfolio</p>
+          </a>
+        </div>
+        <div className='header-right-desktop'>
+          <a className='header-link' href='#projects'>
+            Projects
+          </a>
+          <a className='header-link' href='#'>
+            CV/R&#201;SUM&#201;
+          </a>
+          <a
+            href='https://www.linkedin.com/in/bailey-kitchen-600657363/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='logo'
+              src='assets/images/LI-In-Bug.png'
+              alt='LinkedIn Logo'
+            />
+          </a>
+          <a
+            href='https://iamagamedev.itch.io/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='logo'
+              src='assets/images/itchio-logo-textless-black.svg'
+              alt='itch.io Logo'
+            />
+          </a>
+          <a
+            href='https://github.com/IAmAGameDev'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='logo'
+              src='assets/images/github-mark.svg'
+              alt='GitHub Mark'
+            />
+          </a>
+        </div>
+        <div className='header-right-mobile'>
+          <a
+            href='https://www.linkedin.com/in/bailey-kitchen-600657363/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='logo'
+              src='assets/images/LI-In-Bug.png'
+              alt='LinkedIn Logo'
+            />
+          </a>
+          <span
+            className='material-symbols-outlined'
+            //onclick='changeMobileNav()'
+          >
+            menu
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
